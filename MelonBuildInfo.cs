f2aa7b13f852ce3b9baa2baa3f973646d2fb2ce8
Balint837 <pecsibalint2@gmail.com>
2023-06-20 13:08:39 +0200

    bump version

--- MelonBuildInfo.cs
+++ MelonBuildInfo.cs
@@ -10,7 +10,7 @@ namespace SearchPlusPlus
 
         public const string Author = "PBalint817";
 
-        public const string Version = "4.0.1";
+        public const string Version = "4.0.2";
 
         public const string DownloadLink = "https://github.com/Balint817/SearchPlusPlus";
     }
